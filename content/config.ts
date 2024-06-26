21c2e672d7c3cb2819a47879cb67d523bc7546dd
roll <eskarev@gmail.com>
2024-06-26 07:50:11 +0100

    Added blog for cross-posting

--- content/config.ts
+++ content/config.ts
@@ -1,9 +1,14 @@
 import { docsSchema } from "@astrojs/starlight/schema"
 import { defineCollection } from "astro:content"
+import { blogSchema } from "starlight-blog/schema"
 
 // import { i18nSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-  docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({
+    schema: docsSchema({
+      extend: context => blogSchema(context),
+    }),
+  }),
   // i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 }
